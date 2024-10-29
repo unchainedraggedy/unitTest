24abdad14b8305224e5d7a0db872ed9a185732bd
Ash <redcoolike@gmail.com>
2024-10-29 19:49:51 +0100

    initialCommit

---
+++ src/__test__/test.js
@@ -0,0 +1,17 @@
+import health from '../index';
+
+test('health is between 50 and 15', () => {
+  expect(health({ name: 'Selen', health: 30 })).toBe('wounded');
+  expect(health({ name: 'Brie', health: 43 })).toBe('wounded');
+});
+
+test('health is less than 15', () => {
+  expect(health({ name: 'Erine', health: 10 })).toBe('critical');
+});
+
+test('healthy line', () => {
+  expect(health({ name: 'Elly', health: 14 })).toBe('critical');
+  expect(health({ name: 'Barney', health: 49 })).toBe('wounded');
+  expect(health({ name: 'Robin', health: 51 })).toBe('healthy');
+});
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+export default function health(obj) {
+  if (obj.health < 15) {
+    return 'critical';
+  } if (obj.health < 50) {
+    return 'wounded';
+  }
+  return 'healthy';
+}
