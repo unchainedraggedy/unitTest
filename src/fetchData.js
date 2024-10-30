069f13d42787d87f67cb91dd26a3e8a63f8d101c
Ash <redcoolike@gmail.com>
2024-10-30 15:12:00 +0100

    mocking

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,14 +1,14 @@
 import fetchData from './http';
 
-export function getLevel(userId) {
-    try {
-        const response = JSON.parse(fetchData(`https://server/user/${userId}`));
+export default function getLevel(userId) {
+  try {
+    const response = JSON.parse(fetchData(`https://server/user/${userId}`));
 
-        // Логика обработки
-        if (response.status === 'ok') {
-                return `Ваш текущий уровень: ${response.level}`;
-        }
-            } catch (error) {
-        return `Ошибка при получении данных: ${error.message}`;
+    if (response.status === 'ok') {
+      return `Ваш текущий уровень: ${response.level}`;
     }
+    return 'Информация об уровне временно недоступна';
+  } catch (error) {
+    return `Ошибка при получении данных: ${error.message}`;
+  }
 }
