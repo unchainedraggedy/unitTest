f8ffc5d34ee2553731573ea7bf6c826a53c0167c
Ash <redcoolike@gmail.com>
2024-10-30 14:26:40 +0100

    lintTest

--- src/__test__/sorting.test.js
+++ src/__test__/sorting.test.js
@@ -1,16 +1,16 @@
-import sortCharacters from '../sort.js'
+import sortCharacters from '../sort';
 
 const charactes = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-  ]
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
 
 test('sorting charactes', () => {
-    const result = sortCharacters(charactes)
-    expect(result).toEqual([
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-      ])
-})
+  const result = sortCharacters(charactes);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
